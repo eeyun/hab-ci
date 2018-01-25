1e20d3d1526a5384fa6fae50518699b317e39a7d
Ian Henry <ihenry@chef.io>
2018-01-25 15:01:07 -0500

    Revert recursive testing changes
    Signed-off-by: Ian Henry <ihenry@chef.io>

--- images/hab-delmo/entrypoint.sh
+++ images/hab-delmo/entrypoint.sh
@@ -26,25 +26,5 @@ if [ ! -z ${MACHINE_NAME} ] && [ ! -z ${MACHINE_EXPORT_AWS_ACCESS_KEY_ID} ] \
     docker volume prune -f
 fi
 
-echo "Moving on to Tests."
-echo ""
-echo "Running Tests for ${PKG_CONTEXT} first..."
-echo ""
-exec delmo --only-build-task -f "${PKG_CONTEXT}-tests/${PKG_CONTEXT}/tests/delmo.yml" -m "${MACHINE_NAME}"
-
-if [ -d "./${GROUP_CONTEXT}" ]; then
-    cd "${GROUP_CONTEXT}"
-    build_idents=(`cat ./* | jq '[.group[]| .ident]'| tr -d '[]()""' | tr ',' '\n'| awk NF`)
-    for pkg_ident in "${build_idents[@]}"; do
-        pkg_path="/hab/pkgs/${pkg_ident}"
-        hab pkg install "${pkg_ident}"
-        if [ -f "$pkg_path/tests/delmo.yml" ]; then
-           exec delmo --only-build-task -f "$pkg_path/tests/delmo.yml" -m ${MACHINE_NAME}
-        else
-            echo "No tests in pkg: $ident"
-        fi
-    done
-    else
-        echo "Could not find ${GROUP_CONTEXT}"
-        exit 1
-fi
+echo "Running '$@'"
+exec $@
